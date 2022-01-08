b7fb9eeccc8012e86777d5e0cf278298fa727e9a
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-01-08 19:43:32 +0530

    Create Coins And Triangle.cpp

---
+++ Coins And Triangle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main() {
+int T;
+cin>>T;
+while(T>0){
+    int h;
+    int gc;
+    cin>>gc;
+    for(int i=1;(((i*(i+1))/2)<=gc);i++){
+        
+           h=i;
+    }
+    std::cout << h << std::endl;
+  
+  
+    T=T-1;
+    
+}
+	return 0;
+}
